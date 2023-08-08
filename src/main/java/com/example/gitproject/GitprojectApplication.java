604e64bda3ab8dbcc1facb68f88897f8644add71
Yaocheng Li <rickyli0821@gmail.com>
2023-08-08 13:45:29 -0700

    first2

---
+++ src/main/java/com/example/gitproject/GitprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitprojectApplication.class, args);
+	}
+
+}
